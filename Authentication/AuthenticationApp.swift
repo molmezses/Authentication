866eec29e5861556d4f9927ccfaca21e24d34af8
Mustafa <mustafaolmezses@icloud.com>
2024-09-08 18:07:11 +0300

    Initial Commit

---
+++ Authentication/AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationApp.swift
+//  Authentication
+//
+//  Created by Mustafa Ölmezses on 8.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
